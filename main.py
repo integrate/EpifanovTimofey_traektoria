72eabeafebaf44099800486f0539763be238df76
artem <Football@gmail.com>
2023-02-18 15:30:27 +0300

    traektoria

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import wrap
+import wrap,random
 
 wrap.world.create_world(800, 700)
 wrap.world.set_back_color(100, 200, 10)
@@ -6,35 +6,65 @@ mod = "default"
 tochka = []
 pacman = None
 t2 = 0
+spisok_pacman = []
+spisok_celey_pacman = []
+
+
+
+def delete_tochka():
+    for t in tochka:
+        wrap.sprite.remove(t)
+    tochka.clear()
+
+
+def create_tochka(pos_x, pos_y):
+    t1 = wrap.sprite.add("pacman", pos_x, pos_y)
+    tochka.append(t1)
+
+
+def create_pacman():
+    pacman = wrap.sprite.add("pacman", wrap.sprite.get_x(tochka[0]), wrap.sprite.get_y(tochka[0]), "player2")
+    spisok_pacman.append(pacman)
+    spisok_celey_pacman.append(0)
+
 
 @wrap.on_key_down(wrap.K_d)
-def mods(pos_x,pos_y):
-    global mod,pacman,t2
+def mods(pos_x, pos_y):
+    global mod, pacman, t2
     if mod == "default":
         mod = "tochka"
-        for t in tochka:
-            wrap.sprite.remove(t)
-        tochka.clear()
+        delete_tochka()
         t2 = 0
-        t1 = wrap.sprite.add("pacman",pos_x,pos_y)
-        tochka.append(t1)
+        create_tochka(pos_x, pos_y)
     else:
         mod = "default"
-        pacman = wrap.sprite.add("pacman",wrap.sprite.get_x(tochka[0]),wrap.sprite.get_y(tochka[0]),"player2")
+
+
 
 @wrap.on_mouse_move()
 def tochki(pos_x, pos_y):
     if mod == "tochka":
-        t1 = wrap.sprite.add("pacman", pos_x, pos_y)
-        tochka.append(t1)
+        create_tochka(pos_x, pos_y)
+
 
 @wrap.always(25)
 def pacman1():
     global t2
-    if pacman != None and mod == "default":
-        x = wrap.sprite.get_x(tochka[t2])
-        y = wrap.sprite.get_y(tochka[t2])
-        wrap.sprite.set_angle_to_point(pacman,x,y)
-        wrap.sprite.move_at_angle_point(pacman,x,y,3)
-        if len(tochka)-1 > t2 and wrap.sprite.is_collide_sprite(pacman,tochka[t2]):
-            t2 += 1
+    if mod == "default":
+        for pacman3 in spisok_pacman:
+            nomer_pacman = spisok_pacman.index(pacman3)
+            nomer_tochki = spisok_celey_pacman[nomer_pacman]
+            idtochki = tochka[nomer_tochki]
+            x = wrap.sprite.get_x(idtochki)
+            y = wrap.sprite.get_y(idtochki)
+            wrap.sprite.set_angle_to_point(pacman3, x, y)
+            wrap.sprite.move_at_angle_point(pacman3, x, y, random.randint(1,6))
+            if wrap.sprite.is_collide_sprite(pacman3, idtochki) and len(tochka) > nomer_tochki+1:
+                spisok_celey_pacman[nomer_pacman] += 1
+
+
+@wrap.always(1000)
+def pacman2():
+    if len(tochka) >= 1 and mod == "default":
+        create_pacman()
+
